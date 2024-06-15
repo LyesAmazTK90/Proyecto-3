ab5809e87c9d46c18e5c7ce52895ced0d17a9e5f
Eduardo Salazar <ehsc1997@gmail.com>
2024-06-15 13:50:29 +0100

    Added page config and subheader, hidden label for uploader as it was too small

--- frontend.py
+++ frontend.py
@@ -2,33 +2,39 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Transcripción y Análisis de Audio :mic:")
+st.set_page_config(
+        page_title="Tone Analyzer",
+        page_icon=":loud_sound:",
+        layout="wide",
+    )
+
+st.title("Transcripción y Análisis de Audio :microphone: :notes: :musical_note:")
 
 # Subir el archivo
-uploaded_file = st.file_uploader("Sube tu archivo de audio", type=["wav", "mp3"])
+st.subheader("Sube tu archivo de audio WAV o MP3:")
+uploaded_file = st.file_uploader("Sube tu archivo de audio", type=["wav", "mp3"], label_visibility='hidden')
 
 # Asegurar que se haya subido correctamente
-if uploaded_file is None:
-    raise ValueError("No file provided")
+if uploaded_file is not None:
 
-# Transcripccion (audio a texto)
-transcription = "Output of Transcription Functionality" #transcribe_audio(uploaded_file)
-st.write("Transcripción del audio:")
-st.write(transcription)
+    # Transcripccion (audio a texto)
+    transcription = "Output of Transcription Functionality" #transcribe_audio(uploaded_file)
+    st.write("Transcripción del audio:")
+    st.write(transcription)
 
-tone = "Output of Tone Analyzer" #analyze_tone(uploaded_file)
+    tone = "Output of Tone Analyzer" #analyze_tone(uploaded_file)
 
-st.write("Análisis de tono:")
-if tone == "enfado":
-    st.image("emojis/angry.png")
-elif tone == "neutral":
-    st.image("emojis/neutral.png")
-elif tone == "alegría":
-    st.image("emojis/happy.png")
-else:
-    st.write("Tono no reconocido")
+    st.write("Análisis de tono:")
+    if tone == "enfado":
+        st.image("emojis/angry.png")
+    elif tone == "neutral":
+        st.image("emojis/neutral.png")
+    elif tone == "alegría":
+        st.image("emojis/happy.png")
+    else:
+        st.write("Tono no reconocido")
 
 
-text_analysis = "Output of Text Analyzer" #analyze_text(transcription)
-st.write("Análisis del texto:")
-st.write(text_analysis)
+    text_analysis = "Output of Text Analyzer" #analyze_text(transcription)
+    st.write("Análisis del texto:")
+    st.write(text_analysis)
