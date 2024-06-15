dd0badb1fedb05c93c46e9208022f984fd3fe9da
Eduardo Salazar <ehsc1997@gmail.com>
2024-06-15 14:48:33 +0100

    Texto temporal incluido, se borra al subir archivo y se sustituye por los resultados de cada seccion. Codigo mas limpio

--- frontend.py
+++ frontend.py
@@ -14,27 +14,70 @@ st.title("Transcripción y Análisis de Audio :microphone: :notes: :musical_note
 st.subheader("Sube tu archivo de audio WAV o MP3:")
 uploaded_file = st.file_uploader("Sube tu archivo de audio", type=["wav", "mp3"], label_visibility='hidden')
 
+# Crear columnas para separar contenido y componentes
+c1, c2, c3 = st.columns(3)
+
+# Mensaje temporal mientras usuario sube archivo
+no_result_message = "Suba un archivo de audio WAV o MP3 para ver resultados"
+
+with c1:
+    # Subtitulo
+    st.subheader("Transcripción del audio:")
+
+    # Creacion de texto temporal
+    placeholder1 = st.empty()
+    with placeholder1.container():
+        st.write(no_result_message)
+
+with c2:
+    # Subtitulo
+    st.subheader("Análisis de tono:")
+
+    # Creacion de texto temporal
+    placeholder2 = st.empty()
+    with placeholder2.container():
+        st.write(no_result_message)
+
+with c3:
+    # Subtitulo
+    c3.subheader("Análisis del texto:")
+
+    # Creacion de texto temporal
+    placeholder3 = st.empty()
+    with placeholder3.container():
+        st.write(no_result_message)
+
 # Asegurar que se haya subido correctamente
 if uploaded_file is not None:
 
-    # Transcripccion (audio a texto)
-    transcription = "Output of Transcription Functionality" #transcribe_audio(uploaded_file)
-    st.write("Transcripción del audio:")
-    st.write(transcription)
+    with c1:
+        # Borrar placeholder
+        placeholder1.empty()
 
-    tone = "Output of Tone Analyzer" #analyze_tone(uploaded_file)
+        # Transcripcion
+        transcription = "Output of Transcription Functionality" #transcribe_audio(uploaded_file)
+        c1.write(transcription)
 
-    st.write("Análisis de tono:")
-    if tone == "enfado":
-        st.image("emojis/angry.png")
-    elif tone == "neutral":
-        st.image("emojis/neutral.png")
-    elif tone == "alegría":
-        st.image("emojis/happy.png")
-    else:
-        st.write("Tono no reconocido")
+    with c2:
+        # Borrar placeholder
+        placeholder2.empty()
 
+        # Analisis de tono
+        tone = "Output of Tone Analyzer" #analyze_tone(uploaded_file)
 
-    text_analysis = "Output of Text Analyzer" #analyze_text(transcription)
-    st.write("Análisis del texto:")
-    st.write(text_analysis)
+        if tone == "enfado":
+            st.image("emojis/angry.png")
+        elif tone == "neutral":
+            st.image("emojis/neutral.png")
+        elif tone == "alegría":
+            st.image("emojis/happy.png")
+        else:
+            st.write("Tono no reconocido")
+
+    with c3:
+        # Borrar placeholder
+        placeholder3.empty()
+        
+        # Analisis de texto
+        text_analysis = "Output of Text Analyzer" #analyze_text(transcription)
+        st.write(text_analysis)
